9d3607108fa6d6ab21b711c263804fa61dc88d61
ThomasSchmitz1981 <thomas.schmitz@generali.de>
2017-02-10 23:54:09 +0100

    adds donation link

--- mobile/src/main/java/de/luxe/direction/notifier/MainActivity.java
+++ mobile/src/main/java/de/luxe/direction/notifier/MainActivity.java
@@ -8,6 +8,8 @@ package de.luxe.direction.notifier;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView donate = (TextView) findViewById(R.id.infoTxtDonate);
+        donate.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 
