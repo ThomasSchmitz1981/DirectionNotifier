836bcf132971bc0f60c2fee382e39cf4c8c46655
ThomasSchmitz1981 <thomas.schmitz@generali.de>
2017-03-02 22:30:49 +0100

    adds color to donat link

--- mobile/src/main/java/de/luxe/direction/notifier/MainActivity.java
+++ mobile/src/main/java/de/luxe/direction/notifier/MainActivity.java
@@ -5,6 +5,7 @@
  */
 package de.luxe.direction.notifier;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView donate = (TextView) findViewById(R.id.infoTxtDonate);
+        donate.setBackgroundColor(Color.rgb(204,255,255));
         donate.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
